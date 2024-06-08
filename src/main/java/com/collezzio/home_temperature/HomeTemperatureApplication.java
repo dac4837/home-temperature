7712c5c76f783b5409d2a158005ebd5fa590dbf9
David Collins <dacollins3@gmail.com>
2024-06-07 23:10:00 -0400

    start.spring.io code

---
+++ src/main/java/com/collezzio/home_temperature/HomeTemperatureApplication.java
@@ -0,0 +1,13 @@
+package com.collezzio.home_temperature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeTemperatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeTemperatureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collezzio/home_temperature/HomeTemperatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collezzio.home_temperature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeTemperatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
